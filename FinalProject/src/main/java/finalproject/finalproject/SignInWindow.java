51cebc4a48fc77524ebd823b4b59a73cff7d83f9
Santiago Rivera <49661456+santri94@users.noreply.github.com>
2020-01-31 15:12:41 -0500

    Made SignIn window easier to execute in main

--- FinalProject/src/main/java/finalproject/finalproject/SignInWindow.java
+++ FinalProject/src/main/java/finalproject/finalproject/SignInWindow.java
@@ -157,12 +157,12 @@ public class SignInWindow extends javax.swing.JFrame {
         }
         //</editor-fold>
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new SignInWindow().setVisible(true);
-            }
-        });
+        //--------------------------------------------------------------------------------------------------------------------
+        /* Main Starts HERE */
+        //--------------------------------------------------------------------------------------------------------------------
+        SignInWindow signIn = new SignInWindow();
+        signIn.setVisible(true);
+        
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
